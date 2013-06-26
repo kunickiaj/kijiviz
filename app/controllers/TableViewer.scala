4302572a592e94887795e1f62550f9b2efec6988
Aaron Kimball <aaron@wibidata.com>
2013-06-26 12:24:10 -0700

    Preliminary work on viewing table layouts.
    This is being done in the "table viewer" for now; the functionality
    of this and the "layout viewer" are a bit intertwined.
    
    This walks through table layouts and pretty-prints their locality group and family info.

--- app/controllers/TableViewer.scala
+++ app/controllers/TableViewer.scala
@@ -10,17 +10,21 @@ import org.apache.hadoop.hbase.HBaseConfiguration
 
 import org.kiji.schema.KConstants
 import org.kiji.schema.Kiji
+import org.kiji.schema.KijiTable
 import org.kiji.schema.KijiURI
+import org.kiji.schema.layout.KijiTableLayout
 
 object TableViewer extends Controller {
 
+  private val mConf: Configuration = HBaseConfiguration.create()
+
+  /** GET /tables */
   def index = Action {
-    val conf: Configuration = HBaseConfiguration.create()
 
     // TODO: Parameterize the kiji instance used.
     val kiji: Kiji = Kiji.Factory.open(
         KijiURI.newBuilder().withInstanceName(KConstants.DEFAULT_INSTANCE_NAME).build(),
-        conf)
+        mConf)
 
     // List the tables available in the view.
     val tableNames: Seq[String] = kiji.getTableNames().asScala
@@ -28,6 +32,22 @@ object TableViewer extends Controller {
     kiji.release()
 
     Ok(views.html.tableviewer_index(tableNames))
+  }
+
+
+  /** GET /tables/<tablename> */
+  def show(tableName: String) = Action {
+    // Display the layout for the table.
+
+    val kiji: Kiji = Kiji.Factory.open(
+        KijiURI.newBuilder().withInstanceName(KConstants.DEFAULT_INSTANCE_NAME).build(),
+        mConf)
+
+    val table: KijiTable = kiji.openTable(tableName)
+    val layout: KijiTableLayout = table.getLayout()
+    table.release()
+    kiji.release()
+    Ok(views.html.tableviewer_show(tableName, layout.getDesc()))
   }
 
 }
