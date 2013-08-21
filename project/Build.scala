ad6f3cc413d8b714a342cbd9db93cf1f0d336376
Adam Kunicki <adam@wibidata.com>
2013-08-20 21:35:03 -0700

    updated play and cdh versions

--- project/Build.scala
+++ project/Build.scala
@@ -8,15 +8,15 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
 
-  val hadoopVer = "2.0.0-mr1-cdh4.2.1"
-  val hbaseVer = "0.94.2-cdh4.2.1"
+  val hadoopVer = "2.0.0-mr1-cdh4.3.0"
+  val hbaseVer = "0.94.6-cdh4.3.0"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
     //jdbc,
     //anorm,
-    "org.kiji.schema" % "kiji-schema" % "1.1.0",
-    "org.kiji.platforms" % "kiji-cdh4-platform" % "1.1.0"
+    "org.kiji.schema" % "kiji-schema" % "1.2.1",
+    "org.kiji.platforms" % "kiji-cdh4-platform" % "1.2.0"
 
 //    "org.apache.hadoop" % "hadoop-client"  % hadoopVer,
 //    "org.apache.hadoop" % "hadoop-core"  % hadoopVer,
